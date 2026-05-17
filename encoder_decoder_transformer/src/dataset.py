8ea64e06f055f3ac3c3092de74ff111dee5328ce
Sashreek Kumar <sashreekkumar0808@gmail.com>
2026-05-17 22:39:06 +0530

    bug fixes in dataset.py

--- encoder_decoder_transformer/src/dataset.py
+++ encoder_decoder_transformer/src/dataset.py
@@ -3,14 +3,16 @@ import torch.nn as nn
 from torch.utils.data import Dataset
 
 class BilingualDataset(Dataset):
+
     def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len):
         super().__init__()
         self.seq_len = seq_len
-        self.ds= ds
+
+        self.ds = ds
         self.tokenizer_src = tokenizer_src
         self.tokenizer_tgt = tokenizer_tgt
-        self.src_lang = src_lang       
-        self.tgt_lang = tgt_lang       
+        self.src_lang = src_lang
+        self.tgt_lang = tgt_lang
 
         self.sos_token = torch.tensor([tokenizer_tgt.token_to_id("[SOS]")], dtype=torch.int64)
         self.eos_token = torch.tensor([tokenizer_tgt.token_to_id("[EOS]")], dtype=torch.int64)
@@ -18,10 +20,9 @@ class BilingualDataset(Dataset):
 
     def __len__(self):
         return len(self.ds)
-    
-    
-    def __getitem__(self, index):
-        src_target_pair = self.ds[index]
+
+    def __getitem__(self, idx):
+        src_target_pair = self.ds[idx]
         src_text = src_target_pair['translation'][self.src_lang]
         tgt_text = src_target_pair['translation'][self.tgt_lang]
 
@@ -83,8 +84,7 @@ class BilingualDataset(Dataset):
             "src_text": src_text,
             "tgt_text": tgt_text,
         }
-
-
-    def causal_mask(size):
-        mask = torch.triu(torch.ones(1, size, size, diagonal=1)).type(torch.int)
-        return mask == 0
+    
+def causal_mask(size):
+    mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
+    return mask == 0
